dac0493aa2962bfc54ba979bba1abc7f51f06d08
Rajiv Arora <rajiv.arora@geodata.no>
2021-05-05 19:45:19 +0200

    added firebase settings

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD3Ele_DYybupmNbGvXnIUnFqZNRBMW4vc",
+  authDomain: "crown-clothing-5b36f.firebaseapp.com",
+  projectId: "crown-clothing-5b36f",
+  storageBucket: "crown-clothing-5b36f.appspot.com",
+  messagingSenderId: "101555975171",
+  appId: "1:101555975171:web:669c82eecb46a4a941ca36",
+};
